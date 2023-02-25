73566e0a1b434d4a63dda3ab5fd2e1a563aeb574
kwnad <risnadiaaa@gmail.com>
2023-02-25 18:57:35 +0700

    step 2-6

---
+++ lib/app/modules/dashboard/controllers/dashboard_controller.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+
+import 'package:get/get.dart';
+
+import '../../../data/entertainment_response.dart';
+import '../../../data/headline_response.dart';
+import '../../../data/sports_response.dart';
+import '../../../data/technology_response.dart';
+import '../../../utils/api.dart';
+
+class DashboardController extends GetxController {
+  //TODO: Implement DashboardController
+
+  final _getConnect = GetConnect();
+
+  final count = 0.obs;
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  Future<HeadlineResponse> getHeadline() async {
+    final response = await _getConnect.get(BaseUrl.headline);
+    return HeadlineResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<EntertainmentResponse> getEntertainment() async {
+    final response = await _getConnect.get(BaseUrl.entertainment);
+    return EntertainmentResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<SportsResponse> getSports() async {
+    final response = await _getConnect.get(BaseUrl.entertainment);
+    return SportsResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<TechnologyResponse> getTechnology() async {
+    final response = await _getConnect.get(BaseUrl.entertainment);
+    return TechnologyResponse.fromJson(jsonDecode(response.body));
+  }
+
+  void increment() => count.value++;
+}
--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,11 +1,28 @@
+import 'dart:async';
+
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:ujikom/app/modules/dashboard/views/dashboard_view.dart';
+
+import '../../login/views/login_view.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
 
-  final count = 0.obs;
+  late Timer _pindah;
+  final authToken = GetStorage();
+
   @override
   void onInit() {
+    _pindah = Timer.periodic(
+      const Duration(seconds: 4),
+      (timer) => authToken.read('token') == null
+      ? Get.off(
+        () => const LoginView(),
+        transition: Transition.leftToRight,
+      )
+      : Get.off(() => const DashboardView())
+    );
     super.onInit();
   }
 
@@ -16,8 +33,7 @@ class HomeController extends GetxController {
 
   @override
   void onClose() {
+    _pindah.cancel();
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
